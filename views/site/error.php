3a8226400fbd0f937dcc22e02773464bd752d801
Alexander Schilling <dignityinside@protonmail.com>
2020-11-07 16:07:31 +0100

    Fix error page

--- views/site/error.php
+++ views/site/error.php
@@ -8,17 +8,14 @@
 
 use yii\helpers\Html;
 
-$this->title = Html::encode($name);
+$this->title = 'Ошибка 404. Нет такой страницы';
+$this->params['breadcrumbs'][] = $this->title;
 
 ?>
 <div class="site-error">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
-    </div>
-
-    <p>Системе не удалось найти запрашиваемое действие.</p>
+    <p>Если вы считаете, что страницы нет по нашей вине, напишите нам.</p>
 
 </div>
