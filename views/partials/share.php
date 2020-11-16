7a36a4091d083368272256b21baacc8a4f205681
Alexander Schilling <dignityinside@protonmail.com>
2020-11-16 22:19:01 +0100

    Remove sharpay buttons

--- views/partials/share.php
+++ views/partials/share.php
@@ -1,10 +1,6 @@
-<?php
-
-use app\widgets\Sharpay;
-
-?>
-
 <div class="share">
-    <?= \Yii::t('app', 'share_friends') ?><br>
-    <?= Sharpay::widget(); ?>
+    <p><?= \Yii::t('app', 'share_friends') ?></p>
+    <script src="https://yastatic.net/share2/share.js"></script>
+    <div class="ya-share2" data-curtain data-size="l" data-limit="5"
+         data-services="telegram,vkontakte,twitter,facebook,odnoklassniki,whatsapp,evernote"></div>
 </div>
