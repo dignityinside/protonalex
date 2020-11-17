0a0500701e29c99c8c29e8e148f4849b13266232
Alexander Schilling <dignityinside@protonmail.com>
2020-11-17 20:59:26 +0100

    Remove post header

--- views/post/_view.php
+++ views/post/_view.php
@@ -15,8 +15,6 @@ use yii\helpers\Markdown;
         <h3><?= Html::a($model->title, ['post/view', 'slug' => $model->slug]); ?></h3>
     </div>
 
-    <?= $this->render('_post_header', ['model' => $model]) ?>
-
     <?= Text::cut('[cut]',
         Text::cut('[premium]', HtmlPurifier::process(Markdown::process($model->content, 'gfm'))),
         Html::a('Подробнее →', ['post/view', 'slug' => $model->slug])
