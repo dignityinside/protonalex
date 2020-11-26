89daf5005213da6107005af12b423ea6e6ef640f
Alexander Schilling <dignityinside@protonmail.com>
2020-11-26 20:29:16 +0100

    Change layout

--- views/layouts/blog.php
+++ views/layouts/blog.php
@@ -1,15 +1,15 @@
 <?php $this->beginContent('@app/views/layouts/main.php'); ?>
 
 <div class="row">
-    <div class="col-md-2">
+    <div class="col-md-3">
         <?= $this->render('./partials/sidebar/categories.php'); ?>
         <?= $this->render('./partials/sidebar/subscribe.php'); ?>
         <?php // echo $this->render('./partials/sidebar/social.php'); ?>
     </div>
-    <div class="col-md-8">
+    <div class="col-md-6">
         <?= $content; ?>
     </div>
-    <div class="col-md-2">
+    <div class="col-md-3">
         <?= $this->render('./partials/sidebar/interesting.php'); ?>
         <?= $this->render('./partials/sidebar/useful.php'); ?>
         <?= $this->render('./partials/sidebar/friends.php'); ?>
