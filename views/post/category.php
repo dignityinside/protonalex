e96252996066cdf71dab28cccad34c77aeaac0af
Alexander Schilling <dignityinside@protonmail.com>
2020-12-05 14:45:10 +0100

    SEO optimization

--- views/post/category.php
+++ views/post/category.php
@@ -14,14 +14,27 @@ $categoryName = $category->name;
 $this->title = sprintf('Записи из категории: %s', $categoryName);
 
 $this->registerMetaTag(['name' => 'title', 'content' => $this->title]);
-$this->registerMetaTag(['name' => 'description', 'content' => $this->title]);
-$this->registerMetaTag(['name' => 'robots', 'content' => 'noindex']);
+$this->registerMetaTag(['name' => 'description', 'content' => $category->description]);
+$this->registerMetaTag(['name' => 'robots', 'content' => 'index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1']);
 
-$this->registerLinkTag(['rel' => 'canonical', 'href' => Url::to(['category/' . $category->slug], true)]);
+$currentPage = Url::to(['category/' . $category->slug], true);
+$this->registerLinkTag(['rel' => 'canonical', 'href' => $currentPage]);
+
+/**
+ * https://ogp.me
+ */
+$this->registerMetaTag(['name' => 'og:type', 'content' => 'object']);
+$this->registerMetaTag(['name' => 'og:title', 'content' => $this->title]);
+$this->registerMetaTag(['name' => 'og:description', 'content' => $category->description]);
+$this->registerMetaTag(['name' => 'og:url', 'content' => $currentPage]);
+$this->registerMetaTag(['name' => 'og:site_name', 'content' => \Yii::$app->params['site']['name']]);
+$this->registerMetaTag(['name' => 'og:locale', 'content' => Yii::$app->language]);
+
 ?>
 <div class="post-index">
     <div class="post-header text-center">
         <h1><i class="fas fa-folder"></i> <?= $categoryName; ?></h1>
+        <p><?= $category->description ?></p>
     </div>
     <?php Pjax::begin(); ?>
         <?= ListView::widget(
